84c3385ba3e754128915323956c091835f0a08d7
tdduong <tdduong.khtn@gmail.com>
2014-01-14 13:04:31 +0700

    lên nóc nhà là bắt con gà

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('dashboardSmgApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
   .config(function ($routeProvider) {
     $routeProvider
