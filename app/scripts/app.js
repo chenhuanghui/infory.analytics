69b683c73bcd4315eb020e5d5da3870128cf6c69
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-01-26 12:33:54 +0700

    finish

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,121 @@
 'use strict';
 
 angular.module('dashboardSmgApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'ui.bootstrap'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui.bootstrap'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(['$routeProvider', '$locationProvider', '$httpProvider',
+        function($routeProvider, $locationProvider, $httpProvider) {
+
+            var access = routingConfig.accessLevels;
+
+            $routeProvider.when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl',
+                access: access.user
+            }).when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl',
+                access: access.anon
+            }).when('/register', {
+                templateUrl: 'views/register.html',
+                controller: 'RegisterCtrl',
+                access: access.anon
+            }).when('/private', {
+                templateUrl: 'views/private.html',
+                controller: 'PrivateCtrl',
+                access: access.user
+            }).when('/admin', {
+                templateUrl: 'views/admin.html',
+                controller: 'AdminCtrl',
+                access: access.admin
+            }).when('/404', {
+                templateUrl: '404',
+                access: access.public
+            }).otherwise({
+                redirectTo: '/404'
+            });
+
+            //$locationProvider.html5Mode(true);
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            //delete $httpProvider.defaults.headers.post['Content-type']
+
+            $httpProvider.interceptors.push(function($q, $location) {
+                return {
+                    'responseError': function(response) {
+                        if (response.status === 401 || response.status === 403) {
+                            $location.path('/login');
+                            return $q.reject(response);
+                        } else {
+                            return $q.reject(response);
+                        }
+                    }
+                }
+            });
+
+            $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+            // Override $http service's default transformRequest
+            $httpProvider.defaults.transformRequest = [
+
+                function(data) {
+                    /**
+                     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+                     * @param {Object} obj
+                     * @return {String}
+                     */
+                    var param = function(obj) {
+                        var query = '';
+                        var name, value, fullSubName, subName, subValue, innerObj, i;
+
+                        for (name in obj) {
+                            value = obj[name];
+
+                            if (value instanceof Array) {
+                                for (i = 0; i < value.length; ++i) {
+                                    subValue = value[i];
+                                    fullSubName = name + '[' + i + ']';
+                                    innerObj = {};
+                                    innerObj[fullSubName] = subValue;
+                                    query += param(innerObj) + '&';
+                                }
+                            } else if (value instanceof Object) {
+                                for (subName in value) {
+                                    subValue = value[subName];
+                                    fullSubName = name + '[' + subName + ']';
+                                    innerObj = {};
+                                    innerObj[fullSubName] = subValue;
+                                    query += param(innerObj) + '&';
+                                }
+                            } else if (value !== undefined && value !== null) {
+                                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+                            }
+                        }
+
+                        return query.length ? query.substr(0, query.length - 1) : query;
+                    };
+
+                    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+                }
+            ];
+
+        }
+    ])
+
+.run(['$rootScope', '$location', '$http', 'Auth',
+    function($rootScope, $location, $http, Auth) {
+
+        $rootScope.$on("$routeChangeStart", function(event, next, current) {
+            $rootScope.error = null;
+            if (!Auth.authorize(next.access)) {
+                if (Auth.isLoggedIn()) $location.path('/');
+                else $location.path('/login');
+            }
+        });
+
+    }
+]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,105 @@
 'use strict';
 
 angular.module('dashboardSmgApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
+
+/* Controllers */
+
+angular.module('dashboardSmgApp')
+    .controller('NavCtrl', ['$rootScope', '$scope', '$location', 'Auth',
+        function($rootScope, $scope, $location, Auth) {
+            $scope.user = Auth.user;
+            $scope.userRoles = Auth.userRoles;
+            $scope.accessLevels = Auth.accessLevels;
+
+            $scope.logout = function() {
+                Auth.logout(function() {
+                    $location.path('/login');
+                }, function() {
+                    $rootScope.error = "Failed to logout";
+                });
+            };
+        }
+    ]);
+
+angular.module('dashboardSmgApp')
+    .controller('LoginCtrl', ['$rootScope', '$scope', '$location', '$window', 'Auth',
+        function($rootScope, $scope, $location, $window, Auth) {
+
+            $scope.rememberme = true;
+            $scope.login = function() {
+                Auth.login({
+                        username: $scope.username,
+                        role: $scope.role
+                    },
+                    function(res) {
+                        $location.path('/');
+                    },
+                    function(err) {
+                        $rootScope.error = "Failed to login";
+                    });
+            };
+
+            $scope.loginOauth = function(provider) {
+                $window.location.href = '/auth/' + provider;
+            };
+        }
+    ]);
+
+angular.module('dashboardSmgApp')
+    .controller('HomeCtrl', ['$rootScope',
+        function($rootScope) {
+
+        }
+    ]);
+
+angular.module('dashboardSmgApp')
+    .controller('RegisterCtrl', ['$rootScope', '$scope', '$location', 'Auth',
+        function($rootScope, $scope, $location, Auth) {
+            $scope.role = Auth.userRoles.user;
+            $scope.userRoles = Auth.userRoles;
+
+            $scope.register = function() {
+                Auth.register({
+                        username: $scope.username,
+                        password: $scope.password,
+                        role: $scope.role
+                    },
+                    function() {
+                        $location.path('/');
+                    },
+                    function(err) {
+                        $rootScope.error = err;
+                    });
+            };
+        }
+    ]);
+
+angular.module('dashboardSmgApp')
+    .controller('PrivateCtrl', ['$rootScope',
+        function($rootScope) {}
+    ]);
+
+
+angular.module('dashboardSmgApp')
+    .controller('AdminCtrl', ['$rootScope', '$scope', 'Users', 'Auth',
+        function($rootScope, $scope, Users, Auth) {
+            $scope.loading = true;
+            $scope.userRoles = Auth.userRoles;
+
+            Users.getAll(function(res) {
+                $scope.users = res;
+                $scope.loading = false;
+            }, function(err) {
+                $rootScope.error = "Failed to fetch users.";
+                $scope.loading = false;
+            });
+
+        }
+    ]);
