13ba145f25917aa740aa353890d62d54d4c2bd55
tdduong <tdduong.khtn@gmail.com>
2014-01-29 10:52:10 +0700

    init auth

--- app/app.js
+++ app/app.js
@@ -12,17 +12,103 @@ angular.module('dashboardSmgApp', [
   'account',
   'user'
 ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    var access = rolesConfig.accessLevels;
+
     $routeProvider
       .when('/', {
         templateUrl: 'modules/home/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        access: access.user
       })
+      .when('/404', {
+        templateUrl: '404',
+        access: access.public
+      }).otherwise({
+         redirectTo: '/404'
+      });
+
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    //delete $httpProvider.defaults.headers.post['Content-type']
+
+    $httpProvider.interceptors.push(function($q, $location) {
+       return {
+           'responseError': function(response) {
+               if (response.status === 401 || response.status === 403) {
+                   $location.path('/login');
+                   return $q.reject(response);
+               } else {
+                   return $q.reject(response);
+               }
+           }
+       }
+    });
+ 
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [
+
+    function(data) {
+    /**
+    * The workhorse; converts an object to x-www-form-urlencoded serialization.
+    * @param {Object} obj
+    * @return {String}
+    */
+      var param = function(obj) {
+        var query = '';
+        var name, value, fullSubName, subName, subValue, innerObj, i;
+
+        for (name in obj) {
+         value = obj[name];
+
+         if (value instanceof Array) {
+           for (i = 0; i < value.length; ++i) {
+             subValue = value[i];
+             fullSubName = name + '[' + i + ']';
+             innerObj = {};
+             innerObj[fullSubName] = subValue;
+             query += param(innerObj) + '&';
+           }
+         } else if (value instanceof Object) {
+           for (subName in value) {
+             subValue = value[subName];
+             fullSubName = name + '[' + subName + ']';
+             innerObj = {};
+             innerObj[fullSubName] = subValue;
+             query += param(innerObj) + '&';
+           }
+         } else if (value !== undefined && value !== null) {
+           query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+         }
+        }
+
+        return query.length ? query.substr(0, query.length - 1) : query;
+      };
+
+      return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
   })
 
   .controller('MainCtrl', function ($scope, $rootScope) {
     $rootScope._username = "HAHAHAH";
-  });
+  })
+
+  .run(['$rootScope', '$location', '$http', 'Auth',
+    function($rootScope, $location, $http, Auth) {
+      $rootScope.$on("$routeChangeStart", function(event, next, current) {
+        $rootScope.error = null;
+        if (!Auth.authorize(next.access)) {
+          if (Auth.isLoggedIn()) 
+            $location.path('/');
+          else 
+            $location.path('/login');
+        }
+      });
+    }
+  ]);
 
 
 // Define modules
---
+++ app/common/services/auth.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular.module('dashboardSmgApp')
+    .factory('Auth', function($http, $cookieStore) {
+
+        // Lấy danh sách accessLevels và userRoles ở file rolesConfig.js
+        var accessLevels = rolesConfig.accessLevels,
+            userRoles = rolesConfig.userRoles,
+            currentUser = $cookieStore.get('user') || {
+                username: '',
+                role: userRoles.public
+            };
+
+        $cookieStore.remove('user');
+
+        function changeUser(user) {
+            _.extend(currentUser, user);
+        };
+
+        return {
+            authorize: function(accessLevel, role) {
+                if (role === undefined)
+                    role = currentUser.role;
+
+                return accessLevel.bitMask & role.bitMask;
+            },
+            isLoggedIn: function(user) {
+                if (user === undefined)
+                    user = currentUser;
+                return user.role.title == userRoles.user.title || user.role.title == userRoles.admin.title;
+            },
+            register: function(user, success, error) {
+                $http.post('/register', user).success(function(res) {
+                    changeUser(res);
+                    success();
+                }).error(error);
+            },
+            login: function(user, success, error) {
+
+                $.ajax({
+                    url: 'http://smartguide.dev/loginapi',
+                    type: 'POST',
+                    data: user,
+                    contentType: 'application/x-www-form-urlencoded',
+                    success: function(data) {
+                        changeUser(JSON.parse(data));
+                        success();
+                    },
+                    error: function(data) {
+                        error(data);
+                    }
+                });
+            },
+
+            logout: function(success, error) {
+                $http.post('/logout').success(function() {
+                    changeUser({
+                        username: '',
+                        role: userRoles.public
+                    });
+                    success();
+                }).error(error);
+            },
+            accessLevels: accessLevels,
+            userRoles: userRoles,
+            user: currentUser
+        };
+    });
+
+angular.module('dashboardSmgApp')
+    .factory('Users', function($http) {
+        return {
+            getAll: function(success, error) {
+                $http.get('/users').success(success).error(error);
+            }
+        };
+    });
