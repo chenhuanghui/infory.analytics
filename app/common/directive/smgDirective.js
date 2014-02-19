a2dc6aa71425b25ed9b3dadb38bd4a9f05361dae
chenhuanghui <inbox.huytran@gmail.com>
2014-02-19 15:35:20 +0700

    compile finish

--- app/common/directive/smgDirective.js
+++ app/common/directive/smgDirective.js
@@ -4,8 +4,8 @@ angular.module('smgDirectives')
         $scope.events = remoteFactory.meta_events;
         
     }])
-    .directive('smgFilter', [function () {
-        
+    .directive('smgFilter', function ($compile) {
+        var queryCount = 2;
         return {
             restrict: "A",
             templateUrl: 'common/template/filter.html',
@@ -17,22 +17,28 @@ angular.module('smgDirectives')
                 $scope.addCondition = function() {
                     $actionGroup = $element.find(".action_group");
                     $queryGroup = $element.find(".query_builder");
-                    console.log($scope.metas);
-                    var action = '<select class="action clearfix"> '
+                    
+                    console.log($scope);
+
+                    var action = $compile('<select class="action clearfix row_'+queryCount+'"> '
                                     + '<option>Và</option>'
                                     + '<option>Hoặc</option>'
-                                + '</select>    ';
-                    var query_row = '<div class="query_record">'
-                                    + '<select class="qr_1" ng-model="property" ng-options="property as property.name_display for property in event.properties" ng-init="property=event.properties[0]"></select>'
+                                + '</select>    ')($scope);
+
+
+                    var query_row = $compile('<div class="query_record row_'+ queryCount +'">'
+                                    + '<select class="qr_1" ng-model="property" ng-options="property as property.name_display for property in $scope.event.properties" ng-init="property=event.properties[0]"></select>'
                                     + '<select class="qr_2" ng-model="meta" ng-options="meta as meta for meta in metas[property.type].operators_display" ng-init="meta=metas[property.type].operators_display[0]"></select>'
                                     + '<select class="qr_3">'
                                     +        '<option>Nam</option>'
                                     +        '<option>Nữ</option>'
                                     +    '</select>'
-                                    + '<span class="icon-remove right remove"></span>'
-                                    + '</div>';
+                                    + '<span class="icon-remove right remove" ng-click="removeCondition($event)" data=.row_'+queryCount+'></span>'
+                                    + '</div>')($scope);
                     $actionGroup.append(action);
                     $queryGroup.append(query_row);
+
+                    queryCount++ ;
                 }
                 $scope.removeCondition = function($event) {
                     rowId = $($event.target).attr("data");
@@ -41,4 +47,4 @@ angular.module('smgDirectives')
             }
         };
 
-}])
+    })
