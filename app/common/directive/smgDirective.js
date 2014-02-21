38230a93692422dcee0d71c8f02ca8e10810a2c6
chenhuanghui <inbox.huytran@gmail.com>
2014-02-21 13:51:01 +0700

    fix style

--- app/common/directive/smgDirective.js
+++ app/common/directive/smgDirective.js
@@ -17,14 +17,15 @@ angular.module('smgDirectives')
                 metadata: "="
             },
             link: function(scope, element, attrs) {
-
+                
             }
         };
 
     })
     .directive('subfilter', function($compile) {
         var qc = 1,
-            _meta, _prop, _operator;
+            _meta, _prop
+            _operator = "AND";
 
         return {
             restrict: 'A',
@@ -40,27 +41,33 @@ angular.module('smgDirectives')
                 scope.addCondition = function() {
                     $actionGroup = element.find(".action_group");
                     $queryGroup = element.find(".query_builder");
-                    scope.oprator =
-                        _prop = "property" + qc,
+                    
+                    _prop = "property" + qc,
                     _meta = "meta" + qc;
                     scope.qc = qc;
+                    scope.operator = _operator;
+                    var action_op = $compile('<select ng-model="operator" class="action clearfix row_' + qc + '"> ' + '<option>AND</option>' + '<option>OR</option>' + '</select>    ')(scope);
 
-                    var action_op = $compile('<select ng-model="operator" ng-init="AND" class="action clearfix row_' + qc + '"> ' + '<option>Và</option>' + '<option>Hoặc</option>' + '</select>    ')(scope);
+                    // $compile('<div class="btn-group pull-right action clearfix row_'+qc+'"><button class="glow left active">AND </button><button class="glow right">OR</button></div>')(scope);
 
-
-                    var action_nor = $compile('<span>{{_opra}}</span>')(scope);
+                    var action_nor = $compile('<span class="btn-flat white action action_child clearfix  row_' + qc + '">{{operator}}</span>')(scope);
+                    
                     var query_row = $compile('<div query-record events="events" metas="metas" event="event" metadata="metadata" class="row_' + qc + '" data=".row_' + qc + '"></div>')(scope);
 
-
-                    $actionGroup.append(action_op);
+                    if (element.find(".action").length > 0 ) {
+                        $actionGroup.append(action_nor);    
+                    } else {
+                        $actionGroup.append(action_op);        
+                    }
                     $queryGroup.append(query_row);
 
-                    scope.$watch('operator', function() {
-                        alert('hey, myVar has changed!');
+                    scope.$watch('operator', function(newOperator, oldOperator){
+                        _operator = newOperator;
+                        element.find('.action_child').text(newOperator);
                     })
                     qc++;
                 }
-
+                
 
             }
         };
@@ -79,10 +86,14 @@ angular.module('smgDirectives')
                 },
                 templateUrl: 'common/template/query_record.html',
                 link: function(scope, element, attr) {
-                    scope.date = moment("2013-01-22T00:00:00.000").toDate();
+
                     scope.removeCondition = function($event) {
                         rowId = $($event.target).parent().parent().attr("data");
                         element.parents().find(rowId).remove();
+                        console.log(scope.property + "--" + scope.meta + "--" + scope.usrvalue);
+                    }
+                    scope.getValue = function() {
+                        console.log(scope.property);
                     }
                 }
             };
