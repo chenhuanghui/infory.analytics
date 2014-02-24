adaafd33e16e9213400cd0f13b2e395664bb259f
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-02-24 10:28:07 +0700

    add Auth for account Ctrl

--- app/modules/account/account.js
+++ app/modules/account/account.js
@@ -1,34 +1,43 @@
 angular.module('account')
-	.controller('accountCtrl', ['$scope', function ($scope) {
-		$scope.activeTab = "inbox";
-		$scope.activeTab = "personal";
+    .controller('accountCtrl', ['$scope',
+        function($scope) {
+            $scope.activeTab = "inbox";
+            $scope.activeTab = "personal";
 
-		$scope.cancel = function (){
-			window.history.back();
-		};
-	}])
+            $scope.cancel = function() {
+                window.history.back();
+            };
+        }
+    ])
 
-	.config(function($routeProvider){
-		$routeProvider
-			.when('/inbox', {
-				templateUrl: 'modules/account/inbox.html',
-				controller: 'accountCtrl'
-			})
-			.when('/money', {
-				templateUrl: 'modules/account/money.html',
-				controller: 'accountCtrl'
-			})
-			.when('/personal', {
-				templateUrl: 'modules/account/personal.html',
-				controller: 'accountCtrl'
-			})
-			.when('/signin', {
-				templateUrl: 'modules/account/signin.html',
-				controller: 'accountCtrl'
-			})
-			.when('/signup', {
-				templateUrl: 'modules/account/signup.html',
-				controller: 'accountCtrl'
-			})
+.config(function($routeProvider) {
+    var access = routingConfig.accessLevels;
 
-	});
+    $routeProvider
+        .when('/inbox', {
+            templateUrl: 'modules/account/inbox.html',
+            controller: 'accountCtrl',
+            access: access.user
+        })
+        .when('/money', {
+            templateUrl: 'modules/account/money.html',
+            controller: 'accountCtrl',
+            access: access.user
+        })
+        .when('/personal', {
+            templateUrl: 'modules/account/personal.html',
+            controller: 'accountCtrl',
+            access: access.user
+        })
+        .when('/signin', {
+            templateUrl: 'modules/account/signin.html',
+            controller: 'accountCtrl',
+            access: access.user
+        })
+        .when('/signup', {
+            templateUrl: 'modules/account/signup.html',
+            controller: 'accountCtrl',
+            access: access.user
+        })
+
+});
