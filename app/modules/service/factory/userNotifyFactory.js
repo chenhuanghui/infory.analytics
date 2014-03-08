6245605e6e10079e783d7541470da095714a4afb
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-03-08 14:15:29 +0700

    userNotify completed step 1

---
+++ app/modules/service/factory/userNotifyFactory.js
@@ -0,0 +1,36 @@
+angular.module('smg.services')
+    .factory('userNotifyFactory', ['$http',
+        function($http) {
+            var dataStep1 = null;
+            var dataStep2 = null;
+            var dataStep3 = null;
+            var dataStep4 = null;
+            return {
+                getDataStep1: function() {
+                    return dataStep1;
+                },
+                setDataStep1: function(data) {
+                    dataStep1 = data;
+                },
+                getDataStep2: function() {
+                    return dataStep2;
+                },
+                setDataStep2: function(data) {
+                    dataStep2 = data;
+                },
+                getDataStep3: function() {
+                    return dataStep3;
+                },
+                setDataStep3: function(data) {
+                    dataStep3 = data;
+                },
+                getDataStep4: function() {
+                    return dataStep4;
+                },
+                setDataStep4: function(data) {
+                    dataStep4 = data;
+                },
+
+            }
+        }
+    ])
--- app/modules/user/user.js
+++ app/modules/user/user.js
@@ -162,8 +162,160 @@ angular.module('user')
     }
 ])
 
-.controller('UserNotifyCtrl', ['$scope', 'remoteFactory',
-    function($scope, remoteFactory) {
+.controller('UserNotifyCtrl', ['$scope', '$routeParams', '$location', 'remoteFactory', 'dataFactory', 'userNotifyFactory',
+    function($scope, $routeParams, $location, remoteFactory, dataFactory, userNotifyFactory) {
+
+        var brandId = $routeParams.brandId;
+        var path = $location.path().substring(0, 23);
+        dataFactory.getBrand(brandId, function(data) {
+            $scope.brand = data;
+        }, function() {});
+
+        switch (path) {
+            case '/user/notify-new/step1/':
+                var oldData = userNotifyFactory.getDataStep1();
+
+                $scope.notifyTypes = [{
+                    id: 0,
+                    name: 'sms',
+                    name_display: 'Gửi qua SMS'
+
+                }, {
+                    id: 1,
+                    name: 'email',
+                    name_display: 'Gửi qua email'
+                }, {
+                    id: 2,
+                    name: 'in-app',
+                    name_display: 'Gửi qua ứng dụng'
+                }];
+
+                $scope.isCanGo = false;
+                $scope.validation = [
+                    [true, true],
+                    [true, true, true],
+                    [true]
+                ];
+
+                $scope.isOk = [false, false, false];
+
+                if (oldData != null) {
+                    $scope.isCanGo = oldData.isCanGo;
+                    $scope.validation = oldData.validation;
+                    $scope.notifyType = oldData.notifyType;
+                    $scope.isOk = oldData.isOk;
+                    $scope.sms_sender = oldData.sms_sender;
+                    $scope.sms_content = oldData.sms_content;
+                    $scope.email_title = oldData.email_title;
+                    $scope.email_content = oldData.email_content;
+                    $scope.email_sender = oldData.email_sender;
+                    $scope.in_app_content = oldData.in_app_content;
+                } else
+                    $scope.notifyType = $scope.notifyTypes[0];
+
+                $scope.updateValidation = function(id, idChange) {
+                    switch (id) {
+                        case 0:
+                            switch (idChange) {
+                                case 0:
+                                    $scope.validation[id][idChange] = checkString($scope.sms_sender);
+                                    break;
+                                case 1:
+                                    $scope.validation[id][idChange] = checkString($scope.sms_content);
+                                    break;
+                            }
+                            break;
+                        case 1:
+                            switch (idChange) {
+                                case 0:
+                                    $scope.validation[id][idChange] = checkString($scope.email_title);
+                                    break;
+                                case 1:
+                                    $scope.validation[id][idChange] = checkString($scope.email_sender);
+                                    break;
+                                case 2:
+                                    $scope.validation[id][idChange] = checkString($scope.email_content);
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            $scope.validation[id][idChange] = checkString($scope.in_app_content);
+                            break;
+                    }
+
+                    $scope.isOk[id] = isOk(id);
+                    $scope.updateGoNext();
+
+                }
+
+                $scope.$watch('notifyType', function() {
+                    if ($scope.notifyType != null)
+                        $scope.updateGoNext();
+                });
+
+                $scope.updateGoNext = function() {
+                    for (var i = 0; i < $scope.isOk.length; i++)
+                        if ($scope.isOk[i] == true && $scope.notifyType.id == i) {
+                            $scope.isCanGo = true;
+                            return;
+                        }
+                    $scope.isCanGo = false;
+                }
+
+                function checkString(string) {
+                    if (string == undefined || string == '')
+                        return true;
+                    else
+                        return false;
+                }
+
+                function isOk(id) {
+                    for (var i = 0; i < $scope.validation[id].length; i++) {
+                        if ($scope.validation[id][i] == true)
+                            return false;
+                    }
+                    return true;
+                }
+
+                $scope.goToStep2 = function() {
+                    userNotifyFactory.setDataStep1({
+                        isCanGo: $scope.isCanGo,
+                        isOk: $scope.isOk,
+                        validation: $scope.validation,
+                        notifyType: $scope.notifyType,
+                        sms_sender: $scope.sms_sender,
+                        sms_content: $scope.sms_content,
+                        email_title: $scope.email_title,
+                        email_content: $scope.email_content,
+                        email_sender: $scope.email_sender,
+                        in_app_content: $scope.in_app_content,
+                    });
+                    $location.path('/user/notify-new/step2/' + brandId);
+
+                }
+                break;
+
+            case '/user/notify-new/step2/':
+                $scope.goToStep3 = function() {
+                    $location.path('/user/notify-new/step3/' + brandId);
+                }
+                $scope.goToStep1 = function() {
+                    $location.path('/user/notify-new/step1/' + brandId);
+                }
+                break;
+
+            case '/user/notify-new/step3/':
+                $scope.goToStep4 = function() {
+                    $location.path('/user/notify-new/step2/' + brandId);
+                }
+                $scope.goToStep2 = function() {
+                    $location.path('/user/notify-new/step2/' + brandId);
+                }
+                break;
+
+            case '/user/notify-new/step4/':
+                break;
+        }
         $scope.colorpicker = {
             red: 50,
             options: {
@@ -177,14 +329,7 @@ angular.module('user')
         $scope.events = remoteFactory.meta_events;
         $scope.metadata = remoteFactory.meta_lists;
         $scope.subfilters = [];
-        $scope.notifyTypes = [{
-            id: 1,
-            name: 'Gửi qua SMS'
 
-        }, {
-            id: 2,
-            name: 'Gửi qua email'
-        }];
 
         $scope.data = {
             dateDropDownInput: moment("2013-01-22T00:00:00.000").toDate(),
