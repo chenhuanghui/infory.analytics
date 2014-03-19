fa807bc6a0b379af0ae198f0f1ca5a02586bc280
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-03-19 17:03:21 +0700

    finish account profile update

--- app/modules/account/account.js
+++ app/modules/account/account.js
@@ -1,9 +1,15 @@
 angular.module('account')
-    .controller('AccountCtrl', ['$scope', '$http', '$location', '$routeParams', 'dataFactory', 'Auth', 'accountRemote', '$modal', 'remoteFactory', 'fileHelper',
-        function($scope, $http, $location, $routeParams, dataFactory, Auth, accountRemote, $modal, remoteFactory, fileHelper) {
+    .controller('AccountCtrl', ['$scope', '$http', '$location', '$routeParams', 'dataFactory', 'Auth', 'accountRemote', '$modal', 'remoteFactory', 'fileHelper', 'dialogHelper',
+        function($scope, $http, $location, $routeParams, dataFactory, Auth, accountRemote, $modal, remoteFactory, fileHelper, dialogHelper) {
             /*modal*/
 
             var base_url = remoteFactory.getBaseUrl();
+
+            $scope.isChangePass = false;
+
+            $scope.markPassChange = function() {
+                $scope.isChangePass = true;
+            }
 
             $scope.showBill = function() {
                 var modalInstance = $modal.open({
@@ -52,6 +58,7 @@ angular.module('account')
                 fileHelper.readAsDataUrl($files[0], $scope)
                     .then(function(result) {
                         $scope.account.avatar = result;
+                        fileAvatar = $files[0];
                         //$scope.account.avatar = 'https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-prn1/t1.0-9/s600x600/1904163_10202726642879107_1479781473_n.jpg';
                     });
             }
@@ -60,16 +67,20 @@ angular.module('account')
                 var fields = {
                     name: $scope.account.name,
                     email: $scope.account.email,
-                    company: $scope.account.company,
-                    password: $scope.account.password
+                    company: $scope.account.company
                 };
+
+                if ($scope.isChangePass) {
+                    fields.password = $scope.account.password;
+                }
 
                 if (fileAvatar != null) {
                     var fd = new FormData();
                     fd.append('name', $scope.account.name);
                     fd.append('avatar', fileAvatar);
                     fd.append('company', $scope.account.company);
-                    fd.append('password', $scope.account.password);
+                    if ($scope.isChangePass)
+                        fd.append('password', $scope.account.password);
                     fd.append('email', $scope.account.email);
 
                     var xhr = new XMLHttpRequest();
@@ -77,8 +88,13 @@ angular.module('account')
                     xhr.onreadystatechange = function() {
                         if (xhr.readyState == 4) {
                             var respone = JSON.parse(xhr.responseText);
-                            if (respone.error != undefined) {} else
-                                dialogHelper.showError(data.error.message);
+                            if (respone.error == undefined) {
+                                dialogHelper.showError('Đã cập nhật thay đổi');
+                                $scope.$apply(function() {
+                                    $scope.account.avatar = respone.avatar;
+                                });
+                            } else
+                                dialogHelper.showError(respone.error.message);
                         }
                     }
                     xhr.send(fd);
@@ -86,9 +102,10 @@ angular.module('account')
                     accountRemote.update(fields, function(data) {
                         if (data.error == undefined) {
                             orignalAccount = $scope.account;
-                            $scope.showSuccessNotify = true;
+                            dialogHelper.showError('Đã cập nhật thay đổi');
                         } else {
                             $scope.account = orignalAccount;
+                            dialogHelper.showError(data.error.message);
                         }
                     }, function() {
                         $scope.account = orignalAccount;
