56426049f97502df75d05189e5886eea48f2df6a
tdduong <tdduong.khtn@gmail.com>
2014-03-25 10:21:20 +0700

    fix

--- app/modules/service/remote/systemRemote.js
+++ app/modules/service/remote/systemRemote.js
@@ -3,7 +3,7 @@ angular.module('smg.services')
         function($http, remoteFactory) {
             var base_url = remoteFactory.getBaseUrl() + 'system/';
             return {
-                getParams: function(fields, success, error) {
+                get: function(fields, success, error) {
                     $http.post(base_url + 'get_params' + remoteFactory.getTailUrl(), fields).success(success).error(error);
                 }
             }
