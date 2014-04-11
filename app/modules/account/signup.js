7dc950f84052ca87b2def827fd968982fc1e0fcb
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-04-11 17:16:57 +0700

    sign up not finished

--- app/modules/account/signup.js
+++ app/modules/account/signup.js
@@ -1,7 +1,16 @@
 angular.module('account')
     .controller('signUpCtrl', ['$scope',
         function($scope) {
-            $scope.activeTab = "account";
+            $scope.IsRightPass = false;
+            $scope.password = '';
+            $scope.confirmPassword = '';
+
+            $scope.updatePass = function() {
+                if ($scope.password != $scope.confirmPassword)
+                    $scope.IsRightPass = false;
+                else
+                    $scope.IsRightPass = true;
+            }
         }
     ])
 
