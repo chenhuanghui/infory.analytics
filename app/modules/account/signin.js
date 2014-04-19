648e7c30de544db1184f08bbfe838ce759cb2d74
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-04-19 09:33:17 +0700

    [New] Bổ sung animation loading từ lúc nhấn đăng nhập đến lúc tải được dữ liệu của view home

--- app/modules/account/signin.js
+++ app/modules/account/signin.js
@@ -5,7 +5,7 @@ angular.module('account')
             // $scope.userRoles = Auth.userRoles;
             // $scope.accessLevels = Auth.accessLevels;
             // $scope.hideMessage = true;
-
+            $scope.hideLoading = true;
             $scope.logout = function() {
                 Auth.logout(function() {
                     $location.path('/login');
@@ -15,6 +15,8 @@ angular.module('account')
             };
 
             $scope.login = function() {
+                $scope.hideLoading = false;
+                $scope.hideMessage = true;
                 Auth.login({
                         username: $scope.username,
                         password: $scope.password,
@@ -25,12 +27,13 @@ angular.module('account')
                         dataFactory.setCurrentBrand(null);
                         dataFactory.updateBrandsHeader(null);
                         dataFactory.updateBrandHeader(null);
-
+                        $scope.hideLoading = true;
                         $scope.hideMessage = false;
                         $location.path('/');
                     },
                     function(err) {
                         $scope.message = err;
+                        $scope.hideLoading = true;
                         $scope.hideMessage = false;
                     });
             };
