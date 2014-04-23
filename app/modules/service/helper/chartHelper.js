a0370e97883a78dc82b4cab6f719bdef0b45373c
Sang Nguyen <nncsang@Sangs-MacBook-Pro.local>
2014-04-23 17:07:56 +0700

    fix some errors

--- app/modules/service/helper/chartHelper.js
+++ app/modules/service/helper/chartHelper.js
@@ -50,13 +50,13 @@ angular.module('Smg')
 
                     return chartData;
                 },
-                buildLineChart: function(data, event) {
+                buildLineChartForHome: function(data, event) {
                     var chartData = {
                         chart: {
                             type: 'line'
                         },
                         title: {
-                            text: 'Thống kê lượt ' + event + 'theo thời gian',
+                            text: event,
                             x: -20 //center
                         },
                         subtitle: {
@@ -115,6 +115,127 @@ angular.module('Smg')
 
                     return chartData;
                 },
+                buildLineChart: function(data, event) {
+                    var chartData = {
+                        chart: {
+                            type: 'line'
+                        },
+                        title: {
+                            text: 'Thống kê lượt ' + event + ' theo thời gian',
+                            x: -20 //center
+                        },
+                        subtitle: {
+                            text: 'Nguồn: infory.vn',
+                            x: -20
+                        },
+                        xAxis: {
+                            categories: data.time
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'lượt '
+                            },
+                            plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                        },
+                        tooltip: {
+                            valueSuffix: ' lượt'
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle',
+                            borderWidth: 0
+                        },
+
+                    };
+
+                    if (data.time == undefined) {
+                        chartData.xAxis.categories = data.groups;
+                        chartData.series = [];
+                        chartData.series.push({
+                            name: 'người dùng',
+                            data: data.values
+                        })
+                    } else {
+
+                        if (data.groups == undefined) {
+                            chartData.series = [{
+                                name: event,
+                                data: data.values
+                            }];
+                        } else {
+                            chartData.series = [];
+                            for (var i = 0; i < data.groups.length; i++) {
+                                chartData.series.push({
+                                    name: data.groups[i],
+                                    data: data.values[i]
+                                })
+                            }
+                        }
+                    }
+
+                    return chartData;
+                },
+                buildPieChartForHome: function(data, event) {
+                    if (data.groups == undefined)
+                        return '';
+
+                    var chartData = {
+                        chart: {
+                            plotBackgroundColor: null,
+                            plotBorderWidth: null,
+                            plotShadow: false
+                        },
+                        title: {
+                            text: event,
+                        },
+                        tooltip: {
+                            pointFormat: '{<b>{point.percentage:.1f}%</b>'
+                        },
+                        plotOptions: {
+                            pie: {
+                                allowPointSelect: true,
+                                cursor: 'pointer',
+                                dataLabels: {
+                                    enabled: true,
+                                    color: '#000000',
+                                    connectorColor: '#000000',
+                                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                                }
+                            }
+                        },
+                        series: [{
+                            name: 'Giá trị',
+                            type: 'pie',
+                            data: []
+                        }]
+                    };
+                    if (data.time != undefined) {
+                        for (var i = 0; i < data.groups.length; i++) {
+                            var sum = 0;
+                            for (var j = 0; j < data.values[i].length; j++)
+                                sum += data.values[i][j];
+
+                            chartData.series[0].data.push({
+                                name: data.groups[i],
+                                y: sum
+                            });
+                        }
+                    } else {
+                        for (var i = 0; i < data.groups.length; i++) {
+                            chartData.series[0].data.push({
+                                name: data.groups[i],
+                                y: data.values[i]
+                            });
+                        }
+                    }
+                    return chartData;
+
+                },
                 buildPieChart: function(data, event) {
                     if (data.groups == undefined)
                         return '';
@@ -126,7 +247,7 @@ angular.module('Smg')
                             plotShadow: false
                         },
                         title: {
-                            text: 'Thống kê lượt ' + event + 'theo thời gian',
+                            text: 'Thống kê lượt ' + event + ' theo thời gian',
                         },
                         tooltip: {
                             pointFormat: '{<b>{point.percentage:.1f}%</b>'
@@ -180,7 +301,7 @@ angular.module('Smg')
                             type: 'column'
                         },
                         title: {
-                            text: 'Thống kê lượt ' + event + 'theo thời gian',
+                            text: 'Thống kê lượt ' + event + ' theo thời gian',
                         },
                         subtitle: {
                             text: 'Nguồn: infory.vn',
